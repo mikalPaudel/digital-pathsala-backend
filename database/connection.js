fdfe23088e69cb26f11dba0a2b49d672334b27c7
Mikal <mikalpaudel58@gmail.com>
2025-05-07 13:23:43 +0545

    commented for better understandability of code

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,15 @@
 // we write database connection code here
 
+
+//Sequelize and Datatypes are inside sequlize and require is a kind of importing. 
+// Sequelize is a class
 const {Sequelize,DataTypes} = require("sequelize")
 
 
+// new Sequelize creates object of class Sequelize. 
+// sequelize is the object here not the same one as above sequelize.
 const sequelize = new Sequelize("postgresql://postgres.jsakeazecmmsxijnjzfu:Mikal@Paudel@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
+
 
 sequelize.authenticate()
 .then(()=>{
@@ -13,7 +19,7 @@ sequelize.authenticate()
     console.log("ERROR:"+err)
 })
 
-// Two ways to insert values inside objects
+// Two ways to insert values inside objects (here info is the object)
 const info={
     name: "mikal",
     age: 23
@@ -23,8 +29,12 @@ const info={
 // info.name = "mikal"
 // info.age = 23
 
+// here db is object and db.Sequelize and db.sequelize is the value we want to put inside db.
 const db = {}
+// sequelize:object  and Sequelize is class but not in db.Sequelize and db.sequelize
 db.Sequelize = Sequelize
 db.sequelize = sequelize
 
+
+//export certain object to use in other files
 module.exports = db
